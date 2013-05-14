59c84d70cf4e7f0a95ea16618c6962903d49c704
CarlosLoGa <carlosloga@gmail.com>
2013-05-14 15:14:04 +0200

    e

--- www/negocio/utilitats.js
+++ www/negocio/utilitats.js
@@ -1,4 +1,15 @@
 ﻿
+function phoneGapRun() {
+    if (navigator.userAgent.match(/(iPhone|iPod|iPad|Android|BlackBerry)/)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
 function mensaje(msg) {
-    alert(msg);
+    if(phoneGapRun())
+        navigator.notification.alert(msg, null, 'Atenció');
+    else
+        alert(msg);
 }
